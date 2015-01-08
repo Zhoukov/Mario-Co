d3180121ad95e7be61248ea36f7930d58bf0732c
Zhoukov <tinusie_93@hotmail.com>
2015-01-08 12:31:09 +0100

    Splashscreen added

--- Mario&Co/src/com/example/marioco/MainActivity.java
+++ Mario&Co/src/com/example/marioco/MainActivity.java
@@ -1,7 +1,6 @@
 package com.example.marioco;
 
-import android.app.Activity;
-import android.app.ActionBar;
+import android.app.Activity; 
 import android.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -9,10 +8,13 @@ import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
-import android.os.Build;
+import android.widget.Spinner;
 
 public class MainActivity extends Activity {
 
+	Spinner spinner;
+	
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
